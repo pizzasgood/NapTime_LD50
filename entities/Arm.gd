beda1faec0738fb9a44662169b8b93bac8c66f0a
pizzasgood <pizzasgood@gmail.com>
2022-04-03 15:30:32 -0400

    Improve gui

--- entities/Arm.gd
+++ entities/Arm.gd
@@ -4,7 +4,6 @@ extends Node2D
 onready var sprite := $Sprite
 onready var grab_zone := $GrabZone
 onready var target : KinematicBody2D = get_tree().get_nodes_in_group("player")[0]
-onready var game_over_menu := get_node("/root/Main").find_node("GameOver")
 
 
 func set_flip(value : bool) -> void:
@@ -23,4 +22,4 @@ func _process(_delta: float) -> void:
 func _on_GrabZone_body_entered(body: Node) -> void:
 	if not body.is_in_group("player"):
 		return
-	game_over_menu.game_over()
+	get_node("/root/Main").find_node("GameOver").game_over()
